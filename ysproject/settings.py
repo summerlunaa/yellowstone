78205f480ef767352f1c74f33a0906d5448ca8f9
summerlunaa <rhgus1085@gmail.com>
2021-05-01 15:22:44 +0900

    second commit

--- ysproject/settings.py
+++ ysproject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'ysdiary'
+    'ysdiary',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- ysproject/urls.py
+++ ysproject/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ysdiary import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('new/', views.new, name = "new"),
+    path('detail/<int:index>', views.detail, name="detail"),
+    path('edit/<int:index>', views.edit, name="edit"),
+    path('detail/<int:pk>/delete', views.delete, name="delete")
 ]
